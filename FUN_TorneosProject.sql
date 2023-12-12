36675770f541cb3e7a07865f8e888768c3d59f89
SrJaniot <estebanjaniot10@gmail.com>
2023-12-11 19:20:46 -0500

    valentina

--- FUN_TorneosProject.sql
+++ FUN_TorneosProject.sql
@@ -94,6 +94,80 @@ $$
 LANGUAGE plpgsql;
 
 
+--FUNCIONES PARA LA TABLA tab_datosPersonales -------------------------------------------------------------------------------------------------------------------------------------------------------------
+--VALIDAR ATRIBUTOS DE LA TABLA tab_datosPersonales
+
+CREATE OR REPLACE FUNCTION fun_validar_datosPersonales_insert(wnombre_jugador tab_datosPersonales.nombre_jugador%TYPE, wedad_jugador tab_datosPersonales.edad_jugador%TYPE,
+                             wtel_jugador tab_datosPersonales.tel_jugador%TYPE,wcorreo_jugador tab_datosPersonales.correo_jugador%TYPE,
+                             wfoto_perfil_jugador tab_datosPersonales.foto_perfil_jugador%TYPE,wid_ciudad tab_datosPersonales.id_ciudad%type) RETURNS BOOLEAN AS
+$$
+     DECLARE TAMANIO_NOMBRE INTEGER;
+     DECLARE TAMANIO_NUM_TELEFONO INTEGER;
+     DECLARE ES_CORREO BOOLEAN;
+     DECLARE ID_CIUDAD_ENCONTRADA tab_ciudad.id_ciudad%TYPE;
+     DECLARE CIUDAD_ENCONTRADA BOOLEAN := FALSE;
+
+    BEGIN
+        SELECT CHAR_LENGTH(wnombre_jugador)INTO TAMANIO_NOMBRE;
+        SELECT CHAR_LENGTH(CAST(wtel_jugador AS CHAR)) INTO TAMANIO_NUM_TELEFONO;  
+        IF REGEXP_LIKE(wcorreo_jugador, '[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}') THEN
+            ES_CORREO := TRUE;
+        ELSE
+            ES_CORREO := FALSE;
+        END IF;
+        SELECT id_ciudad INTO ID_CIUDAD_ENCONTRADA FROM tab_ciudad WHERE tab_ciudad.id_ciudad = wid_ciudad;
+        IF ID_CIUDAD_ENCONTRADA IS NOT NULL THEN
+             CIUDAD_ENCONTRADA = TRUE;
+        END IF;
+
+
+        --ESTE IF TIENE QUE VALIDAR : TAMANIONOMBRE>2, WEDAD_JUGADOR=>18  TAMANIO_NUM_TELEFONO=10,ES_CORREO=TRUE, CIUDAD_ENCONTRADA=TRUE 
+        IF TAMANIO_NOMBRE>2 AND wedad_jugador>=18 AND TAMANIO_NUM_TELEFONO=10 AND ES_CORREO AND CIUDAD_ENCONTRADA  THEN
+            RETURN TRUE;
+        ELSE
+            RETURN FALSE;
+        END IF;
+    END;
+$$
+LANGUAGE plpgsql;
+
+
+--FUNCION PARA INSERTAR DATOS EN LA TABLA tab_datosPersonales CON PARAMETROS DE ENTRADA
+CREATE OR REPLACE FUNCTION fun_insert_datosPersonales(wnombre_jugador tab_datosPersonales.nombre_jugador%TYPE, wedad_jugador tab_datosPersonales.edad_jugador%TYPE,
+                             wtel_jugador tab_datosPersonales.tel_jugador%TYPE,wcorreo_jugador tab_datosPersonales.correo_jugador%TYPE,
+                             wfoto_perfil_jugador tab_datosPersonales.foto_perfil_jugador%TYPE,wid_ciudad tab_datosPersonales.id_ciudad%type) RETURNS BOOLEAN AS
+$$
+    DECLARE
+        wnombre_jugador_aux tab_datosPersonales.nombre_jugador%TYPE;
+        wtel_jugador_aux tab_datosPersonales.tel_jugador%TYPE;
+        wcorreo_jugador_aux tab_datosPersonales.correo_jugador%TYPE;
+        wfoto_perfil_jugador_aux tab_datosPersonales.foto_perfil_jugador%TYPE;
+        ULTIMOID INTEGER;
+    BEGIN
+        SELECT UPPER(wnombre_jugador) INTO wnombre_jugador_aux;
+        SELECT CAST(wtel_jugador AS CHAR) INTO wtel_jugador_aux;
+        SELECT LOWER(wcorreo_jugador) INTO wcorreo_jugador_aux;
+        SELECT funcion_Retorna_ultmoid('tab_datosPersonales','id_datosPersonales') INTO ULTIMOID;
+        IF fun_validar_datosPersonales_insert(wnombre_jugador_aux, wedad_jugador,wtel_jugador_aux,wcorreo_jugador_aux,wfoto_perfil_jugador,wid_ciudad) THEN
+            INSERT INTO tab_datosPersonales VALUES (ULTIMOID,wnombre_jugador_aux, wedad_jugador,wtel_jugador_aux,wcorreo_jugador_aux,wfoto_perfil_jugador,wid_ciudad);
+            RETURN TRUE;
+        ELSE
+            RETURN FALSE;
+        END IF;
+    END;
+$$
+LANGUAGE plpgsql;
+
+
+
+
+
+  
+
+
+
+
+
 
 
 
